2697c75e78ffaf5a7e916b2055db24b0cb047ac2
kuprik23 <martkrupik@gmail.com>
2024-01-19 23:20:44 +0000

    app

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ scene.add(light);
 // Load your .obj file
 const objLoader = new OBJLoader();
 objLoader.load('models/computerroom.obj', function (object) {
-  scene.add(object);
-  // Add loaded object to collidableMeshList
-  collidableMeshList.push(object);
+ scene.add(object);
+ // Add loaded object to collidableMeshList
+ collidableMeshList.push(object);
 });
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -31,24 +31,24 @@ controls.dampingFactor = 0.25;
 controls.screenSpacePanning = false;
 
 function animate() {
-  requestAnimationFrame(animate);
-  controls.update();
+ requestAnimationFrame(animate);
+ controls.update();
 
-  // Check for collisions
-  for (var vertexIndex = 0; vertexIndex < camera.geometry.attributes.position.array.length; vertexIndex++) {
-     var localVertex = new THREE.Vector3().fromBufferAttribute(camera.geometry.attributes.position, vertexIndex).clone();
-     var globalVertex = localVertex.applyMatrix4(camera.matrix);
-     var directionVector = globalVertex.sub(camera.position);
+ // Check for collisions
+ for (var vertexIndex = 0; vertexIndex < camera.geometry.attributes.position.array.length; vertexIndex++) {
+    var localVertex = new THREE.Vector3().fromBufferAttribute(camera.geometry.attributes.position, vertexIndex).clone();
+    var globalVertex = localVertex.applyMatrix4(camera.matrix);
+    var directionVector = globalVertex.sub(camera.position);
 
-     var ray = new THREE.Raycaster(camera.position, directionVector.clone().normalize());
-     var collisionResults = ray.intersectObjects(collidableMeshList);
-     if (collisionResults.length > 0 && collisionResults[0].distance < directionVector.length()) {
-        // A collision occurred, stop movement
-        controls.stop();
-     }
-  }
+    var ray = new THREE.Raycaster(camera.position, directionVector.clone().normalize());
+    var collisionResults = ray.intersectObjects(collidableMeshList);
+    if (collisionResults.length > 0 && collisionResults[0].distance < directionVector.length()) {
+       // A collision occurred, stop movement
+       controls.stop();
+    }
+ }
 
-  renderer.render(scene, camera);
+ renderer.render(scene, camera);
 }
 
 animate();
